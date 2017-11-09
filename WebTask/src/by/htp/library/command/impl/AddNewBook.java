13e4f76887cb734db44d04d637d8e41627ec88de
nataliavg <natalinna@tut.by>
2017-11-09 13:17:10 +0300

    cleaning code

--- WebTask/src/by/htp/library/command/impl/AddNewBook.java
+++ WebTask/src/by/htp/library/command/impl/AddNewBook.java
@@ -21,40 +21,44 @@ import by.htp.library.service.factory.ServiceFactory;
 public class AddNewBook implements Command {
 	private static final String NAME_BOOK = "nameBook";
 	private static final String NAME_WRITER= "writer";
+	private static final String GENRE= "genre";
+	private static final String HOUSE= "house";
+	private static final String YEAR= "year";
+	private static final String FILE= "file";
 	private static final String ERROR_MESSAGE = "errorMessage";
-	private static final String MESSAGE_FAILING_ADDITION  = "The book is not add!";
 	private static final String MESSAGE_ABOUT_PROBLEM = "Sorry,technical problem";
-	private static final String MESSAGE_SUCCESSFUL_ADDITION = "&Message=Book successful addition in library!";
-	private static final String URL_VIEW_BOOK="http://localhost:8080/WebTask/Controller?command=viewBook&id=+";
+	private static final String CONTENT_TYPE_TEXT_HTML= "text/html;charset=UTF-8";
+	//private static final String MESSAGE_SUCCESSFUL_ADDITION = "&Message=Book successful addition in library!";
+	private static final String URL_VIEW_BOOK="http://localhost:8080/WebTask/Controller?command=viewBook&id=";
 	private static final String MAIN_JSP = "WEB-INF/jsp/main.jsp";
-	private static final String ERROR_JSP = "error.jsp";
-	private Random random = new Random();
-    
+	private static final String PATH_IMAGE = "C:/Users/Dima/git/Library-Project/WebTask/WebContent/resources/images/";
+	private static final String JPG= ".jpg";
 	
+	private Random random = new Random();
 	
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		 response.setContentType("text/html;charset=UTF-8");
-		 final String Writer=request.getParameter("writer");
-		 final String nameBook=request.getParameter("nameBook");
-		 final String genre=request.getParameter("genre");
-		 final String house=request.getParameter("house");
-		 final String year =request.getParameter("year");
-		 final Part filePart=request.getPart("file");
+		 response.setContentType(CONTENT_TYPE_TEXT_HTML);
+		 final String Writer=request.getParameter(NAME_WRITER);
+		 final String nameBook=request.getParameter(NAME_BOOK);
+		 final String genre=request.getParameter(GENRE);
+		 final String house=request.getParameter(HOUSE);
+		 final String year =request.getParameter(YEAR);
+		 final Part filePart=request.getPart(FILE);
 		   
 		 File uploadetFile = null;
 	
 		 String pathImage;
-		// String nameImage;
+		 String page = null;
 	
-		//выбираем файлу имя пока не найдём свободное
+		//we choose to the file a name we won't find free yet
 		 do{
-			 pathImage = ("C:/Users/Dima/git/Library-Project/WebTask/WebContent/resources/images/" + random.nextInt(1000)+".jpg");	
+			 pathImage = (PATH_IMAGE + random.nextInt(1000)+JPG);	
 				uploadetFile = new File(pathImage);		
 			}while(uploadetFile.exists());
 		
-			//создаём файл
+			//create file
 		 uploadetFile.createNewFile(); 
 		 
 		 OutputStream out = null;
@@ -71,43 +75,28 @@ public class AddNewBook implements Command {
 
 		        while ((read = filecontent.read(bytes)) != -1) {
 		            out.write(bytes, 0, read);
-		        }
-		       
-		        
-		        
+		        	}
 		       
 				ServiceFactory factory=ServiceFactory.getInstance();
 				BookService bookService=factory.getBookService();
 				
-				Book book = null;
-				String page = null;
-				try {
-					book = bookService.addBook( Writer,nameBook,pathImage,genre,house,year);
-					 System.out.println(book.getId());
-					if (book!=null)	{
-						int i=book.getId();
-						String url=URL_VIEW_BOOK+i;
-						String url2=url+MESSAGE_SUCCESSFUL_ADDITION;
-						 System.out.println( url);
-						 
-					response.sendRedirect(url);
+				Book book = null;				
+				
+				book = bookService.addBook( Writer,nameBook,pathImage,genre,house,year);
+					
+				int i=book.getId();
+				String url=URL_VIEW_BOOK+i;
+				
+				response.sendRedirect(url);
 						
-					}
-					//else{
-					//	request.setAttribute(ERROR_MESSAGE, MESSAGE_FAILING_ADDITION);
-					//	page=MAIN_JSP;
-					//	RequestDispatcher dispatcher=request.getRequestDispatcher(page);
-					//	dispatcher.forward(request, response);
-					//}
 				} catch (ServiceException e) {
-					System.out.println( "catch");
 					request.setAttribute(ERROR_MESSAGE, MESSAGE_ABOUT_PROBLEM);
 					page=MAIN_JSP;
 					RequestDispatcher dispatcher=request.getRequestDispatcher(page);
 					dispatcher.forward(request, response);
 					}	
 						        
-		    } finally {
+		    finally {
 		        if (out != null) {
 		            out.close();
 		        }
@@ -117,7 +106,9 @@ public class AddNewBook implements Command {
 		        if (writer != null) {
 		            writer.close();
 		        }
-		    }}}
+		   }
+	}
+}
 	
 	
         
